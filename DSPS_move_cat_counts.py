68302b5796068aa6c8ccd78c031a6cd6111ba494
Rishi Kumar <rishik0208@gmail.com>
2023-09-19 22:27:29 -0700

    a

--- DSPS_move_cat_counts.py
+++ DSPS_move_cat_counts.py
@@ -77,7 +77,13 @@ fig.update_layout(
 
 # Enable scroll zoom
 config = {'scrollZoom': True}
-fig.show(config=config)
+
+# Save the figure as an HTML file
+html_file_name = "status_moves_chart.html"
+pyo.plot(fig, filename=html_file_name, auto_open=False, config=config)
+
+# Display the HTML file path
+print(f"Chart saved as: {html_file_name}")
 
 
 import os
@@ -159,8 +165,13 @@ fig.update_layout(
 
 # Enable scroll zoom
 config = {'scrollZoom': True}
-fig.show(config=config)
 
+# Save the figure as an HTML file
+html_file_name = "runswitch_moves_chart.html"
+pyo.plot(fig, filename=html_file_name, auto_open=False, config=config)
+
+# Display the HTML file path
+print(f"Chart saved as: {html_file_name}")
 
 import os
 import plotly.graph_objects as go
@@ -241,7 +252,13 @@ fig.update_layout(
 
 # Enable scroll zoom
 config = {'scrollZoom': True}
-fig.show(config=config)
+
+# Save the figure as an HTML file
+html_file_name = "priority_moves_chart.html"
+pyo.plot(fig, filename=html_file_name, auto_open=False, config=config)
+
+# Display the HTML file path
+print(f"Chart saved as: {html_file_name}")
 
 
 import os
@@ -331,8 +348,13 @@ fig.update_layout(
 
 # Enable scroll zoom
 config = {'scrollZoom': True}
-fig.show(config=config)
 
+# Save the figure as an HTML file
+html_file_name = "entry_hazard_clear_moves_chart.html"
+pyo.plot(fig, filename=html_file_name, auto_open=False, config=config)
+
+# Display the HTML file path
+print(f"Chart saved as: {html_file_name}")
 
 import os
 import plotly.graph_objects as go
@@ -421,4 +443,10 @@ fig.update_layout(
 
 # Enable scroll zoom
 config = {'scrollZoom': True}
-fig.show(config=config)
+
+# Save the figure as an HTML file
+html_file_name = "entry_hazard_moves_chart.html"
+pyo.plot(fig, filename=html_file_name, auto_open=False, config=config)
+
+# Display the HTML file path
+print(f"Chart saved as: {html_file_name}")
