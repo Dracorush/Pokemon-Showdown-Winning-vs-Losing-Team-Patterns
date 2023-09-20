947ea0d26e6914f99ec8e8204f093db5fe0667c5
Rishi Kumar <rishik0208@gmail.com>
2023-09-19 22:34:52 -0700

    Revert "temp"
    This reverts commit 5970574374849e1b8126cee32772ad2ee9cf3234.

--- DSPS_move_cat_counts.py
+++ DSPS_move_cat_counts.py
@@ -1,8 +1,6 @@
 import os
 import plotly.graph_objects as go
-import plotly.offline as pyo
-
-
+print("Hello World")
 # Directory containing the text files
 directory = "gen9ou-stat_summary"
 
@@ -79,13 +77,7 @@ fig.update_layout(
 
 # Enable scroll zoom
 config = {'scrollZoom': True}
-
-# Save the figure as an HTML file
-html_file_name = "status_moves_chart.html"
-pyo.plot(fig, filename=html_file_name, auto_open=False, config=config)
-
-# Display the HTML file path
-print(f"Chart saved as: {html_file_name}")
+fig.show(config=config)
 
 
 import os
@@ -167,13 +159,8 @@ fig.update_layout(
 
 # Enable scroll zoom
 config = {'scrollZoom': True}
+fig.show(config=config)
 
-# Save the figure as an HTML file
-html_file_name = "runswitch_moves_chart.html"
-pyo.plot(fig, filename=html_file_name, auto_open=False, config=config)
-
-# Display the HTML file path
-print(f"Chart saved as: {html_file_name}")
 
 import os
 import plotly.graph_objects as go
@@ -254,13 +241,7 @@ fig.update_layout(
 
 # Enable scroll zoom
 config = {'scrollZoom': True}
-
-# Save the figure as an HTML file
-html_file_name = "priority_moves_chart.html"
-pyo.plot(fig, filename=html_file_name, auto_open=False, config=config)
-
-# Display the HTML file path
-print(f"Chart saved as: {html_file_name}")
+fig.show(config=config)
 
 
 import os
@@ -350,13 +331,8 @@ fig.update_layout(
 
 # Enable scroll zoom
 config = {'scrollZoom': True}
+fig.show(config=config)
 
-# Save the figure as an HTML file
-html_file_name = "entry_hazard_clear_moves_chart.html"
-pyo.plot(fig, filename=html_file_name, auto_open=False, config=config)
-
-# Display the HTML file path
-print(f"Chart saved as: {html_file_name}")
 
 import os
 import plotly.graph_objects as go
@@ -445,10 +421,4 @@ fig.update_layout(
 
 # Enable scroll zoom
 config = {'scrollZoom': True}
-
-# Save the figure as an HTML file
-html_file_name = "entry_hazard_moves_chart.html"
-pyo.plot(fig, filename=html_file_name, auto_open=False, config=config)
-
-# Display the HTML file path
-print(f"Chart saved as: {html_file_name}")
+fig.show(config=config)
